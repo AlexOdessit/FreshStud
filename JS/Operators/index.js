7d8f0347b5e8d5817175363ca3f5705dd68d0e4a
Alex <alexgofresh@gmail.com>
2023-11-12 13:57:46 +0100

    part 1

--- JS/Operators/index.js
+++ JS/Operators/index.js
@@ -1,1 +1,25 @@
-alert('hello world');
+//Sum
+let num1 = 40;
+let num2 = 60;
+let sum = num1 + num2;
+console.log(sum);
+
+//Difference
+let differ = sum - 50;
+console.log(differ);
+
+//multiplication
+let product = sum * differ;
+console.log(product);
+
+//division
+let quotient = product / 2.687;
+console.log(quotient);
+
+//remainder
+let remainder = product % 2.687;
+console.log(remainder);
+
+// to boolean
+let boolean = !!product;
+console.log(boolean);
