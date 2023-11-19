7b3b490d0be513b570a14ca04143753109a75c96
Alex <alexgofresh@gmail.com>
2023-11-19 21:42:31 +0100

    2parts of conditional task

---
+++ Conditional/conditional.js
@@ -0,0 +1,30 @@
+// Lowest Num
+const findLowNum = function (num1, num2) {
+  let arr2 = [];
+  let arr1 = [num1, num2];
+  for (let i = 1; i < arr1.length; i++) {
+    let min = arr1[0];
+    if (arr1[i] < min) {
+      min = arr1[i];
+    }
+    arr2.push(min);
+  }
+
+  return arr2;
+};
+const lowNum = findLowNum(10, 20);
+console.log(lowNum);
+
+// Check Adult
+// const checkMyAge = function (age) {
+//   if (age >= 18) {
+//     alert('Ok,thank you !!!');
+//   } else if (age !== age) {
+//     alert('Only numeric allowed!');
+//   } else {
+//     alert('Not allowed to enter');
+//   }
+//   return age;
+// };
+// const checkAgeMessage = +prompt('Are you adult?');
+// const ageResult = checkMyAge(checkAgeMessage);
