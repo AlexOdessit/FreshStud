c14eba71d94bff5ff689ca0be24a16406e1ab2ed
Alex <alexgofresh@gmail.com>
2023-11-21 17:06:42 +0100

    changed task

--- Conditional/conditional.js
+++ Conditional/conditional.js
@@ -1,65 +1,65 @@
 // Lowest Num
-// const findLowNum = function (num1, num2) {
-//   let arr2 = [];
-//   let arr1 = [num1, num2];
-//   for (let i = 1; i < arr1.length; i++) {
-//     let min = arr1[0];
-//     if (arr1[i] < min) {
-//       min = arr1[i];
-//     }
-//     arr2.push(min);
-//   }
-
-//   return arr2;
-// };
-// const lowNum = findLowNum(5, 10);
-// console.log(lowNum);
-
-// // // Check Adult
-// const checkMyAge = function (age) {
-//   if (age >= 18) {
-//     alert('Ok,thank you !!!');
-//   } else if (age !== age) {
-//     alert('Only numeric allowed!');
-//   } else if (age < 18) {
-//     alert('Not allowed to enter');
-//   }
-//   return age;
-// };
-
-// let checkAgeMessage;
-// checkAgeMessage === null || checkAgeMessage === ''
-//   ? (checkAgeMessage = +prompt('Please enter your age'))
-//   : (checkAgeMessage = +prompt('Are you adult?'));
-
-// checkMyAge(checkAgeMessage);
-
-// Calculator
-let result = prompt('What would you  like to do (*,/,+,-)');
-let x = +prompt('Enter first Number');
-let y = +prompt('Enter second Number');
-const calcFunction = function (x, y) {
-  switch (result) {
-    case '+':
-      return x + y;
-
-    case '-':
-      return x - y;
-
-    case '/':
-      return x / y;
-
-    case '*':
-      return x * y;
-
-    case null:
-    case 'string':
-      alert('Not correct format');
-      break;
-
-    default:
-      return 'Somethig goes wrong';
+const findLowNum = function (num1, num2) {
+  let arr2 = [];
+  let arr1 = [num1, num2];
+  for (let i = 1; i < arr1.length; i++) {
+    let min = arr1[0];
+    if (arr1[i] < min) {
+      min = arr1[i];
+    }
+    arr2.push(min);
   }
+
+  return arr2;
+};
+const lowNum = findLowNum(5, 10);
+console.log(lowNum);
+
+// // Check Adult
+const checkMyAge = function (age) {
+  if (age >= 18) {
+    alert('Ok,thank you !!!');
+  } else if (age !== age) {
+    alert('Only numeric allowed!');
+  } else if (age < 18) {
+    alert('Not allowed to enter');
+  }
+  return age;
 };
 
-alert(calcFunction(x, y));
+let checkAgeMessage;
+checkAgeMessage === null || checkAgeMessage === ''
+  ? (checkAgeMessage = +prompt('Please enter your age'))
+  : (checkAgeMessage = +prompt('Are you adult?'));
+
+checkMyAge(checkAgeMessage);
+
+// Calculator
+// let result = prompt('What would you  like to do (*,/,+,-)');
+// let x = +prompt('Enter first Number');
+// let y = +prompt('Enter second Number');
+// const calcFunction = function (x, y) {
+//   switch (result) {
+//     case '+':
+//       return x + y;
+
+//     case '-':
+//       return x - y;
+
+//     case '/':
+//       return x / y;
+
+//     case '*':
+//       return x * y;
+
+//     case null:
+//     case 'string':
+//       alert('Not correct format');
+//       break;
+
+//     default:
+//       return 'Somethig goes wrong';
+//   }
+// };
+
+// alert(calcFunction(x, y));
